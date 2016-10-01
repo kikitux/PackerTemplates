59ab3ba8fc4fc0fe5617d2e832d5958d6a9dcf5f
Alvaro Miranda Aguilera <kikitux@gmail.com>
2016-10-01 17:56:36 +0200

    test sharedfolder for cache

--- files/scripts/install_oracle_guest_additions.ps1
+++ files/scripts/install_oracle_guest_additions.ps1
@@ -1,4 +1,3 @@
 Write-Host "Installing Guest Additions"
 certutil -addstore -f "TrustedPublisher" D:\cert\oracle-vbox.cer
-Start-Process -FilePath "D:\VBoxWindowsAdditions.exe" -ArgumentList "/S" -WorkingDirectory "C:\Windows\Temp\virtualbox" -Wait
-Remove-Item "C:\Windows\Temp\virtualbox"
+Start-Process -FilePath "D:\VBoxWindowsAdditions.exe" -ArgumentList "/S" -Wait
